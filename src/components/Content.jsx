a42cdf12c016e8da4e373329e9300019b9491adf
Pollyanna Teixeira Santos <pollyteisantos@gmail.com>
2023-05-01 19:32:50 -0300

    mudança de tema do site

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -67,8 +67,8 @@ export function Content() {
   return (
     <>
       <Header
-        title='Mulheres em Tech Brasil'
-        subtitle='Conheça personalidades femininas que estão transformando a tecnologia no Brasil'
+        title='Lésbicas da ficção'
+        subtitle='Conheça personagens lésbicas do audiovisual e literatura'
         image={listaImg}
       />
       <div className={styles.projectsContainer}>
@@ -94,7 +94,7 @@ export function Content() {
         </div>
       </div>
       <div >
-        <h2 className={styles.projectsTitle}>Cadastre uma rainha tech:</h2>
+        <h2 className={styles.projectsTitle}>Cadastre sua personagem lésbica favorita:</h2>
         <form  className={styles.form} onSubmit={handleCreateMessage}>
           <input 
             onChange={handleInputValueNome} 
